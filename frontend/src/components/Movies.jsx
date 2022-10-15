5d0630214f41519a37df5286a08d29e3b59d8e77
oussama-zemzemi <oussama.zem99@gmail.com>
2022-10-15 20:48:36 +0400

    i added images with map

--- frontend/src/components/Movies.jsx
+++ frontend/src/components/Movies.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import React, { Fragment ,useEffect, useState} from 'react'
 import {AiFillPlayCircle} from 'react-icons/ai'
+import NoImg from './Noimage.jpg'
 
 const Movies = () => {
   const[movieData, setMovieData]=useState([])
   const Api = "https://api.themoviedb.org/3/discover/movie"
+  const Images ="https://image.tmdb.org/t/p/w500"
   const MovieCall = async () => {
     const data = await axios.get(Api,{
      params: {
@@ -22,11 +24,15 @@ const Movies = () => {
   return (
     <Fragment>
       {movieData.map((movie)=> {
+        return(
         <Fragment>
           <div id='container'>
           <AiFillPlayCircle color='green' fontSize={40} id="playIcon"/>
+          <img src={movieData.poster_path ? `${Images}${movieData.poster_path}` : NoImg} alt='' />
+          <h3>{movieData.title}</h3>
           </div>
           </Fragment>
+        )
 
       })}
     </Fragment>
